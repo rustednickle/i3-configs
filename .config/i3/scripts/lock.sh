6e32958774bc252e59f055b0ddaa755a32cda098
Parth Shah <parth@linux.ibm.com>
2020-06-14 19:01:09 +0530

    Adds i3-lock blurring script and changeVolume dunstify interface

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+scrot /tmp/screenshot.png
+TMPBG=/tmp/screenshot.png
+LOCK=$HOME/.config/i3/scripts/lock.png
+RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+ 
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+i3lock -e -u -n -i $TMPBG
+rm $TMPBG
 
-if [[ -f $HOME/.config/i3/scripts/lock.png ]] 
-then
-    # placement x/y
-    PX=0
-    PY=0
-    # lockscreen image info
-    R=$(file $HOME/.config/i3/scripts/lock.png | grep -o '[0-9]* x [0-9]*')
-    RX=$(echo $R | cut -d' ' -f 1)
-    RY=$(echo $R | cut -d' ' -f 3)
-
-    SR=$(xrandr --query | grep ' connected' | cut -f3 -d' ')
-    for RES in $SR
-    do
-        # monitor position/offset
-        SRX=$(echo $RES | cut -d'x' -f 1)                   # x pos
-        SRY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 1)  # y pos
-        SROX=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 2) # x offset
-        SROY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 3) # y offset
-        PX=$(($SROX + $SRX/2 - $RX/2))
-        PY=$(($SROY + $SRY/2 - $RY/2))
-
-        convert /tmp/screen.png $HOME/.config/i3/scripts/lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
-        echo "done"
-    done
-fi 
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 # i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
-i3lock -e -u -n -i /tmp/screen.png
